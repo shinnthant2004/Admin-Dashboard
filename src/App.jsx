2c9d1b297c7c215915765ac44723f10c84322ad2
shinnthant2004 <shinthantequi@gmail.com>
2023-03-21 13:32:11 +0630

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-red-300">Test</h2>
+    </div>
+  );
+}
+
+export default App;

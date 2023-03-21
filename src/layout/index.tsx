f146e20c2426f2e78a7fb271c304561bad413139
shinnthant2004 <shinthantequi@gmail.com>
2023-03-21 15:18:52 +0630

    layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
-        <div>
-            <h2>Layout</h2>
+        <div className="w-screen h-screen bg-red-300">
+          <div className="w-full h-full grid grid-cols-10">
+            <div className="w-full h-full col-span-2 bg-black">
+                SideBar
+            </div>
+            <div className="w-full h-full col-span-8 bg-yellow-50">
+                <Outlet/>
+            </div>
+          </div>
         </div>
     )
 }
